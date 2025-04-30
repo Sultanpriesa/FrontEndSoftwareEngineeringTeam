d224476c4a7616889fa084f8d1a3e3788b315db7
Jomar <jom.nacorda@gmail.com>
2025-04-30 17:29:15 +1200

    Add Schedule, Apply, and Index routes; update routing structure in main.jsx

--- Timetable-app/src/main.jsx
+++ Timetable-app/src/main.jsx
@@ -9,17 +9,23 @@ import {
 import Login from "./Routes/Login";
 import Root from "./Routes/Root";
 import ErrorPage from "./error-page";
+import Schedule from "./routes/Schedule";
+import Index from "./routes/Index";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<Root />} errorElement={<ErrorPage />}>
       <Route index element={<Login />} />
-      <Route path="dashboard" element={<ProtectedRoute requireAdmin />}>
-        <Route index element={<Dashboard />} />
+      <Route path="Schedule" element={<ProtectedRoute requireAdmin />}>
+        <Route index element={<Schedule />} />
       </Route>
       <Route path="settings" element={<ProtectedRoute requireAdmin />}>
         <Route index element={<Settings />} />
       </Route>
+      <Route path="Apply" element={<ProtectedRoute requireAdmin />}>
+        <Route index element={<Apply />} />
+      </Route>
+      
       <Route path="*" element={<ErrorPage />} />
     </Route>
   )
